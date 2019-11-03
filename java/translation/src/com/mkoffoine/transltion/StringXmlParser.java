da06c8c7a55017dd17f1dc0d3e478809f4676d35
MMM <myhayloffpr@gmail.com>
2019-11-04 00:49:07 +0200

    add fix for bad xml

--- java/translation/src/com/mkoffoine/transltion/StringXmlParser.java
+++ java/translation/src/com/mkoffoine/transltion/StringXmlParser.java
@@ -5,14 +5,18 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class StringXmlParser {
@@ -24,10 +28,19 @@ public class StringXmlParser {
         File file = new File(fileName);
         if (DEBUG) System.out.println(file.getAbsolutePath());
 
-        DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
-                .newDocumentBuilder();
+        DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-        Document doc = dBuilder.parse(file);
+        Document doc = null;
+        try {
+             doc = dBuilder.parse(file);
+        }catch (SAXParseException saxEx) {
+            File tmpFile = new File(fileName+"tmp.xml");
+            Files.copy(file.toPath(), tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            fixFile(tmpFile, saxEx);
+            doc = dBuilder.parse(tmpFile);
+        }
+
+
 
         if (DEBUG) System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 
@@ -70,6 +83,40 @@ public class StringXmlParser {
         }
 
         return map;
+    }
+    static void fixFile(File tmpFile, SAXParseException saxEx) throws ParserConfigurationException, IOException, SAXException {
+        System.out.println("/////////"+saxEx.toString()+"----");
+        int lineNumberIndex = saxEx.toString().indexOf("lineNumber");
+        int colomn = saxEx.toString().indexOf(";",lineNumberIndex);
+        int lineNumber = Integer.parseInt(saxEx.toString().substring(lineNumberIndex+12, colomn));
+        System.out.println("---"+lineNumber);
+        Scanner scanner = new Scanner(tmpFile);
+        int i = 0;
+        String badLine = "";
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            i++;
+            if (i == lineNumber) {
+                badLine = line;
+                String name = line.substring(line.indexOf("\""), line.indexOf("\"",line.indexOf("\"")+1));
+                System.out.println(" bad ---- name -- "+name);
+                break;
+            }
+
+        }
+        scanner.close();
+        String fixedContent = Files.readString(tmpFile.toPath()).replace(badLine, " <!-- nononono bad line -->");
+        Files.writeString(tmpFile.toPath(),fixedContent);
+
+
+        DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = null;
+        try {
+            doc = dBuilder.parse(tmpFile);
+        }catch (SAXParseException sxEx) {
+            fixFile(tmpFile, sxEx);
+        }
+
     }
 
 }
